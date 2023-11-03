62879f5e25623c3a501ed6ccdb04262d54386877
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-03 21:06:26 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿int [,] GenerateArray()
+{
+
+    int[,] array = new int[10,10];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}        
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SummStringArrayCheck(int [,] array)
+{
+    int [] summ = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summ[i] = summ[i] + array[i,j];
+            
+
+        }
+        Console.WriteLine($"Cумма cтроки №{i} = {summ[i]} ");
+    }
+    int min = summ[0];
+    int IndexMin = 0;
+    for(int i = 0; i < summ.Length ; i++)
+    {
+        if(summ[i] < min )
+        {
+            min = summ[i];
+            IndexMin = i;
+        }
+    }
+    Console.WriteLine($"Номер строки с минимальной суммой - №{IndexMin}");
+}
+
+int[,] arr = GenerateArray();
+PrintArray(arr);
+SummStringArrayCheck(arr);
